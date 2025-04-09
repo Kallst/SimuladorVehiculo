cb7bcd0b0c81d4a53ff6754157f841c765f0bd71
Santiago <Sant987ago@gmail.com>
2025-04-09 14:08:40 -0500

    Se actualizan clases

--- SimuladorVehiculo/src/autonoma/simuladorvehiculo/models/Motor.java
+++ SimuladorVehiculo/src/autonoma/simuladorvehiculo/models/Motor.java
@@ -2,30 +2,10 @@ package autonoma.simuladorvehiculo.models;
 
 import autonoma.simuladorvehiculo.exceptions.ExcederMotorException;
 
-public abstract class Motor {
-    protected String tipo;
-    protected int velocidadMaxima;
+public interface Motor {
+    int obtenerVelocidadMaxima();
+    String obtenerTipo();
 
-    public Motor(String tipo) {
-        this.tipo = tipo;
-        if (tipo.equals("1.0")) {
-            velocidadMaxima = 120;
-        } else if (tipo.equals("1.6")) {
-            velocidadMaxima = 160;
-        } else if (tipo.equals("2.0")) {
-            velocidadMaxima = 200;
-        } else {
-            velocidadMaxima = 100; // por defecto
-        }
-    }
-
-    public abstract void ();
-
-    public int obtenerVelocidadMaxima() {
-        return velocidadMaxima;
-    }
-
-    public String obtenerTipo() {
-        return tipo;
-    }
+    // Nueva función para validar velocidad
+    void verificarVelocidad(int velocidad) throws ExcederMotorException;
 }
