dc84f6da34ea10ced5049da77984d5b157a90903
Kallst <santiago.castrom@autonoma.edu.co>
2025-04-09 11:03:29 -0500

    Se crea la clase abstracta Llanta

---
+++ SimuladorVehiculo/src/autonoma/simuladorvehiculo/models/Llanta.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.simuladorvehiculo.models;
+
+/**
+ *
+ * @author LAPTOP
+ */
+public abstract class Llanta {
+    public abstract int getLimiteVelocidad();
+    public abstract String getTipo();
+}
+
