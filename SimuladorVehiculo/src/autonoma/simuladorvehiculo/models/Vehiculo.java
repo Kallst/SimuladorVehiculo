f37c606147794b1f345f9923945c78d3fd466d4f
Santiago <Sant987ago@gmail.com>
2025-04-09 11:36:59 -0500

    Se agrega motor a la clase vehiculo

--- SimuladorVehiculo/src/autonoma/simuladorvehiculo/models/Vehiculo.java
+++ SimuladorVehiculo/src/autonoma/simuladorvehiculo/models/Vehiculo.java
@@ -3,11 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package autonoma.simuladorvehiculo.models;
+import autonoma.simuladorvehiculo.models.Llanta;
 
 /**
  *
  * @author sant9
  */
 public class Vehiculo {
+    private Llanta llanta;
+    int limite = llanta.getLimiteVelocidad();
+    String tipo = llanta.getTipo();
+    
+    private Motor motor;
+    int velocidadMax = motor.obtenerVelocidadMaxima();
+    String tipoMotor = motor.obtenerTipo(); 
+    
     
 }
